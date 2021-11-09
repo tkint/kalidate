075dee9cd4a67dd58d7544977e71d307b8c98412
Thomas Kint <thomaskint.pro@gmail.com>
2021-11-09 18:44:32 +0100

    refacto: try simplification

--- lib/src/main/kotlin/com/thomaskint/kalidate/form/KForm.kt
+++ lib/src/main/kotlin/com/thomaskint/kalidate/form/KForm.kt
@@ -1,41 +1,25 @@
 package com.thomaskint.kalidate.form
 
 import com.thomaskint.kalidate.ValidationError
-import com.thomaskint.kalidate.ValidationError.Type.Builtin
-import com.thomaskint.kalidate.form.fields.KAny
-import com.thomaskint.kalidate.form.fields.KBoolean
-import com.thomaskint.kalidate.form.fields.KDouble
 import com.thomaskint.kalidate.form.fields.KField
-import com.thomaskint.kalidate.form.fields.KInteger
-import com.thomaskint.kalidate.form.fields.KString
 
 class KForm private constructor(private val fields: List<KField<*>>) {
     fun validate(data: Map<String, Any?>): List<ValidationError> = fields.map { field ->
-        val value = data[field.name]
-        when {
-            field is KString && value is String? -> field.validate(value)
-            field is KBoolean && value is Boolean? -> field.validate(value)
-            field is KInteger && value is Int? -> field.validate(value)
-            field is KDouble && value is Double? -> field.validate(value)
-            field is KAny -> field.validate(value)
-            else -> listOf(field.error(Builtin.BAD_FORMAT))
-        }
+        field.validateAny(data[field.name])
     }.flatten()
 
     fun validate(vararg data: Pair<String, Any?>) = validate(mapOf(*data))
 
     class Builder {
-        private val fields: MutableList<KField.Builder<*, *>> = mutableListOf()
+        val fields: MutableList<KField.Builder<*>> = mutableListOf()
 
-        fun <DataType, FieldType : KField<DataType>, BuilderType : KField.Builder<DataType, FieldType>> field(builder: BuilderType): BuilderType =
-            builder.also { fields.add(it) }
+        inline fun <reified DataType : Any> field(name: String): KField.Builder<DataType> =
+            KField.Builder(name, DataType::class).also { fields.add(it) }
 
-        fun field(name: String): KAny.Builder = KAny.Builder(name).also { fields.add(it) }
-
-        fun string(name: String): KString.Builder = field(KString.Builder(name))
-        fun boolean(name: String): KBoolean.Builder = field(KBoolean.Builder(name))
-        fun integer(name: String): KInteger.Builder = field(KInteger.Builder(name))
-        fun double(name: String): KDouble.Builder = field(KDouble.Builder(name))
+        fun string(name: String): KField.Builder<String> = field(name)
+        fun boolean(name: String): KField.Builder<Boolean> = field(name)
+        fun integer(name: String): KField.Builder<Int> = field(name)
+        fun double(name: String): KField.Builder<Double> = field(name)
 
         fun build(): KForm = KForm(fields.map { it.build() })
     }
--- lib/src/main/kotlin/com/thomaskint/kalidate/form/fields/KField.kt
+++ lib/src/main/kotlin/com/thomaskint/kalidate/form/fields/KField.kt
@@ -2,30 +2,68 @@ package com.thomaskint.kalidate.form.fields
 
 import com.thomaskint.kalidate.ValidationError
 import com.thomaskint.kalidate.ValidationError.Type.Builtin
+import kotlin.reflect.KClass
 
-abstract class KField<DataType : Any> protected constructor(
+class KField<DataType : Any> private constructor(
     val name: String,
+    private val clazz: KClass<DataType>,
     private val validators: List<(value: DataType?) -> ValidationError?>,
 ) {
     fun validate(value: DataType?): List<ValidationError> = validators.mapNotNull { it(value) }
 
-    fun error(type: ValidationError.Type) = ValidationError(type, name)
+    fun validateAny(value: Any?): List<ValidationError> =
+        if (value == null) validate(null)
+        else if (!clazz.isInstance(value)) listOf(error(Builtin.BAD_FORMAT))
+        else validate(value as DataType)
 
-    abstract class Builder<DataType : Any, FieldType : KField<DataType>>(val name: String) {
-        protected val validators: MutableList<(value: DataType?) -> ValidationError?> = mutableListOf()
+    private fun error(type: ValidationError.Type) = ValidationError(type, name)
 
-        fun required(): Builder<DataType, FieldType> =
+    class Builder<DataType : Any>(private val name: String, private val clazz: KClass<DataType>) {
+        private val validators: MutableList<(value: DataType?) -> ValidationError?> = mutableListOf()
+
+        fun required(): Builder<DataType> =
             spec(Builtin.REQUIRED) { it == null }
 
-        fun spec(fn: (value: DataType?) -> ValidationError?): Builder<DataType, FieldType> =
+        fun spec(fn: (value: DataType?) -> ValidationError?): Builder<DataType> =
             apply { validators.add(fn) }
 
-        fun spec(type: ValidationError.Type, fn: (value: DataType?) -> Boolean): Builder<DataType, FieldType> =
+        fun spec(type: ValidationError.Type, fn: (value: DataType?) -> Boolean): Builder<DataType> =
             spec { if (fn(it)) error(type) else null }
 
-        protected fun error(type: ValidationError.Type): ValidationError =
-            ValidationError(type, name)
+        fun error(type: ValidationError.Type): ValidationError = ValidationError(type, name)
 
-        abstract fun build(): FieldType
+        fun build(): KField<DataType> = KField(name, clazz, validators)
     }
 }
+
+@JvmName("stringMin")
+fun KField.Builder<String>.min(min: Int): KField.Builder<String> =
+    spec(Builtin.TOO_SMALL) { it?.length < min }
+
+@JvmName("stringMax")
+fun KField.Builder<String>.max(max: Int): KField.Builder<String> =
+    spec(Builtin.TOO_BIG) { it?.length > max }
+
+@JvmName("integerMin")
+fun KField.Builder<Int>.min(min: Int): KField.Builder<Int> =
+    spec(Builtin.TOO_SMALL) { it < min }
+
+@JvmName("integerMax")
+fun KField.Builder<Int>.max(max: Int): KField.Builder<Int> =
+    spec(Builtin.TOO_BIG) { it > max }
+
+@JvmName("doubleMin")
+fun KField.Builder<Double>.min(min: Double): KField.Builder<Double> =
+    spec(Builtin.TOO_SMALL) { it < min }
+
+@JvmName("doubleMax")
+fun KField.Builder<Double>.max(max: Double): KField.Builder<Double> =
+    spec(Builtin.TOO_BIG) { it > max }
+
+@JvmName("doubleMinDecimals")
+fun KField.Builder<Double>.minDecimals(decimals: Int): KField.Builder<Double> =
+    spec(Builtin.BAD_FORMAT) { it?.decimalPartSize < decimals }
+
+@JvmName("doubleMaxDecimals")
+fun KField.Builder<Double>.maxDecimals(decimals: Int): KField.Builder<Double> =
+    spec(Builtin.BAD_FORMAT) { it?.decimalPartSize > decimals }
